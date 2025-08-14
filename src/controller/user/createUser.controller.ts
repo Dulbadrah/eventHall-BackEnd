3d7c82c1d6a4ae85054e076565fb516c8be79f7d
25LP2339 25LP2339 <25LP2339@pinecones-iMac-2.local>
2025-08-14 12:43:23 +0800

    1

--- src/controller/user/createUser.controller.ts
+++ src/controller/user/createUser.controller.ts
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import { Response, Request } from "express";
-import { prisma } from "../../utils/prisma";
-import bcrypt from "bcrypt";
-
-export const createUser = async (req: Request, res: Response) => {
-  try {
-    const { name, email, password, role } = req.body;
-
-    if (!name || !email || !password) {
-      return res.status(400).json({ message: "Name, email, password шаардлагатай" });
-    }
-
-    const existingUser = await prisma.user.findUnique({
-      where: { email }
-    });
-
-    if (existingUser) {
-      return res.status(409).json({ message: "Email аль хэдийн бүртгэлтэй байна" });
-    }
-
- 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await prisma.user.create({
-      data: {
-        name:name,
-        email:email,
-        password: hashedPassword,
-        role:role, 
-      },
-    });
-console.log(newUser)
-    res.status(200).json({ message: "User created successfully", user: newUser });
-  } catch (error) {
-    console.error(error);
-=======
 import { Request, Response } from "express";
 import { PrismaClient, Role } from "@prisma/client";
 import bcrypt from "bcrypt";
@@ -64,7 +27,6 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(201).json({ message: "User created successfully", user });
   } catch (error) {
     console.error("Create user error:", error);
->>>>>>> 18d2958 (1)
     res.status(500).json({ message: "Internal server error" });
   }
 };
